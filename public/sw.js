7c05ec19933a08c10550e507670138b133f37ada
David Adokuru <davidadokuru@gmail.com>
2020-01-18 09:32:52 +0100

    updates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,14 @@
 //install sw
+// install event
 self.addEventListener("install", evt => {
-	console.log("service worker installed");
+  console.log("service worker installed");
+});
+
+// activate event
+self.addEventListener("activate", evt => {
+  console.log("service worker activated");
+});
+
+self.addEventListener("fetch", evt => {
+  console.log("service worker activated", evt);
 });
