42228f4cbeaed56354e8f787668b1421b063ac1a
David Adokuru <davidadokuru@gmail.com>
2020-01-26 00:46:27 +0100

    Updates to app.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -215,6 +215,6 @@ $(function() {
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker
 		.register("/sw.js")
-		.then(reg => console.log("Service Worker Registered", reg))
+		.then(reg => console.log("Service Worker Registered"))
 		.catch(err => console.log("Service Worker Not Registered", err));
 }
--- public/sw.js
+++ public/sw.js
@@ -1,32 +1,30 @@
 // cache
-const staticCacheName = "site-static";
-const assets = [
-	"/",
-	"/index.html",
-	"/assets/js/main.js",
-	"/assets/js/bootstrap.min.js",
-	"/assets/css/all.min.css",
-	"/assets/css/styles.min.css",
-	"/assets/css/default.min.css",
-	"/assets/css/animate.min.css",
-	"/assets/css/slick.min.css",
-	"/assets/css/bootstrap.min.min.css.min.css",
-	"/assets/images/danoitech/favicon.ico"
-	"/assets/images/logo.png",
+const staticCacheName = "DanoiCache";
+
+const cacheitems = [
+	"js/main.js",
+	"assets/js/bootstrap.min.js",
+	"assets/css/all.min.css",
+	"assets/css/styles.min.css",
+	"assets/css/default.min.css",
+	"assets/css/animate.min.css",
+	"assets/css/slick.min.css",
+	"assets/css/bootstrap.min.min.css.min.css",
+	"assets/images/danoitech/favicon.ico",
+	"assets/images/logo.png"
 ];
 
 // install event
 
 self.addEventListener("install", evt => {
- //console.log('service worker installed');
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
+	//console.log('service worker installed');
+	evt.waitUntil(
+		caches.open(staticCacheName).then(cache => {
+			console.log("caching shell assets");
+			cache.addAll(cacheitems);
+		})
+	);
 });
-
 
 // activate event
 self.addEventListener("activate", evt => {
